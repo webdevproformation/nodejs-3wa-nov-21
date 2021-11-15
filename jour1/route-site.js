55e77850e4d90b7fefe7e0673cfe64bc9885f329
Malik H <malik.h@webdevpro.net>
2021-11-15 15:48:25 +0100

    réaliser la requête POST du formulaire

--- jour1/route-site.js
+++ jour1/route-site.js
@@ -10,8 +10,18 @@ router.get("/home" , (req, rep) => {
 })
 
 router.get("/add-user" , (req, rep) => {
+    const www = `${req.protocol}://${req.headers.host}${req.originalUrl}`;
+    //            http          ://   localhost:3444/   add-user
+    rep.render("add-user" , { www });
+})
 
-    rep.render("add-user");
+router.post("/add-user" , (req, rep) => {
+    const { email, password } = req.body;
+
+    rep.json( { 
+        status : "success" ,
+        profil : {email, password}
+    })
 })
 
 module.exports = router ;
