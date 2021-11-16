9336bfcaef9418c9e6588eeb3fceaafe143e5977
Malik H <malik.h@webdevpro.net>
2021-11-16 11:05:52 +0100

    web socket

---
+++ jour2/index.js
@@ -0,0 +1,29 @@
+// websocket 
+// technologie qui permet de créer connexion bidirectionnelle entre le client et le serveur
+
+// HTTP 
+// client (requete www) => serveur reçoit la demande => traité 
+// serveur envoye la réponse sur client => client traite la réponse 
+
+// vers 2010 couche sur le procole HTTP via websocket 
+// client (requete www) => serveur reçoit la demande => traité 
+// serveur envoye la réponse sur client => client traite la réponse 
+// la connexion reste ouverte entre les deux client / serveur 
+// serveur peut envoyer des informations au client SANS qu'il fasse de requête 
+
+// Uber => passé votre commande => attendez le taxi 
+// carte le véhicule se déplace tout seul 
+// pas besoin de besoin d'appuyer sur un bouton refresh 
+
+// multicast => serveur va pouvoir envoyer des informations à plusieurs clients en même temps
+
+// application de tchat 
+// client 1      <=========== >
+// client 2      <=========== > 1 serveur 
+// client 3      <=========== >
+
+// broadcast => serveur va pouvoir envoyer des informations à plusieurs clients en même temps
+
+// client 1 
+// client 2      <=========== > 1 serveur 
+// client 3      <===========
