db28b01a449d8d842362e1b001717d205a42e016
Malik H <malik.h@webdevpro.net>
2021-11-17 16:39:53 +0100

    modification script jest

--- jour3-integration/index.js
+++ jour3-integration/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4444 ;
 
 const url = process.env.NODE_ENV === "production" ? process.env.BDD : process.env.BDD_DEV
 
-connect(url , {useNewUrlParser : true})
+connect(url , {useNewUrlParser : true} )
     .then(( ) => console.log("connexion bdd " + url))
     .catch(ex => console.log(new Error(ex)))
 
@@ -15,3 +15,7 @@ app.use(express.json());
 app.use("/" , require("./route"));
 
 app.listen(PORT , console.log(`express sur port ${PORT}`));
+
+// export NODE_ENV=production && nodemon index.js
+// postman => via cet outil vous venez de réaliser un test manuel de votre API 
+// test integration manuellement code + dépendance à la base de données 
