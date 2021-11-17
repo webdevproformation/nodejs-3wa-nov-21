14c9a0b340de76343dc904effcd15de2ce7fd7c5
Malik H <malik.h@webdevpro.net>
2021-11-17 17:34:09 +0100

    fin du premier test d'intégration

--- jour3-integration/test/model-article.test.js
+++ jour3-integration/test/model-article.test.js
@@ -1,20 +1,24 @@
 const request = require("supertest");
 const server = require("../index");
+const { Article } = require("../model-article")
 let serveur ; 
 
 describe("POST " , () => {
     beforeEach(() => { // permet d'effectuer une traitement AVANT chaque test
         serveur = require("../index"); // démarrer le serveur
     })
-    afterEach( () => { // permet d'effectuer une traitement APRES chaque test
+    afterEach( async () => { // permet d'effectuer une traitement APRES chaque test
+        await Article.deleteMany({}) // permet d'effectuer des tests => GET 
         serveur.close(); // stopper le serveur 
     })
     it("va ajouter un nouvel article dans la base de données " , async () => {
        const rep = await request(server).post("/").send({titre : "Article1" , contenu : "lorem ipsum" })
-        // console.log(rep); 
+       expect(rep.body).toHaveProperty("_id");
+       expect(rep.body).toHaveProperty("__v");
+       expect(rep.body).toMatchObject({titre : "Article1"})
+       expect(rep.body).toMatchObject({contenu : "lorem ipsum"})
     })
-    it("va ajouter un nouvel article dans la base de données " , async () => {
-        const rep = await request(server).post("/").send({titre : "Article2" , contenu : "lorem ipsum" })
-         // console.log(rep); 
-    })
+    
+
+
 }) 
