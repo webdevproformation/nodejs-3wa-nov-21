5621ca58f7a96ae3d6a4e642c47902d90ae5cbc2
Malik H <malik.h@webdevpro.net>
2021-11-19 16:44:48 +0100

    tache fusionnée

---
+++ jour5-gulp/gulpfile.js
@@ -0,0 +1,53 @@
+// npm i gulp gulp-autoprefixer -save-dev
+
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const uglifycss = require("gulp-uglifycss");
+
+gulp.task("addPrefix" , async () => {
+    await gulp.src("./style.css")
+            .pipe( autoprefixer({
+                browsers: ['last 99 versions']
+            }) )
+            .pipe( gulp.dest("prod") )
+})
+// gulp addPrefix 
+gulp.task("compress" , async () => {
+    await gulp.src("./style.css")
+            .pipe(uglifycss())
+            .pipe( gulp.dest("prod") )
+})
+// gulp compress
+
+// const runSequence = require("run-sequence");
+
+// fusionner dans une seule tâche 
+//  npm i run-sequence --save-dev
+gulp.task("prodCss" , async() => {
+    await gulp.src("./style.css")
+            .pipe( autoprefixer({
+                browsers: ['last 99 versions']
+            }) )
+            .pipe(uglifycss())
+            .pipe( gulp.dest("prod") )
+})
+// fusionner manuellement les deux taches 
+
+/* gulp.task("prodCss2" , async(cb) => {
+    const addPrefix = async function(){
+        await gulp.src("./style.css")
+            .pipe( autoprefixer({
+                browsers: ['last 99 versions']
+            }) )
+        cb()
+    }
+    const compress = async (cb) => {
+        await gulp.src("./style.css")
+            .pipe(uglifycss())
+        cb()
+    }
+
+    await gulp.series(addPrefix , compress)
+                .pipe( gulp.dest("prod") ) ;
+}) */
+
