04dc6aa9409f5e786b5fcff9ef61305e99ddf38b
Malik H <malik.h@webdevpro.net>
2021-11-19 14:44:06 +0100

    filtrer les uploads

--- jour5-upload/server.js
+++ jour5-upload/server.js
@@ -7,8 +7,26 @@ const multer = require("multer");
 const PORT = process.env.PORT || 3210
 const app = express()
 // middleware 
-const upload = multer({dest : "images/"}) // chemin relatif dans le dossier en cours 
 
+const strategie = multer.diskStorage({
+    destination : (req, file , cb) => {
+        cb( null , `${__dirname}/images/` ) // chemin absolu dans le dossier en cours 
+    },
+    filename : (req, file , cb) => {
+        cb(null , `${Date.now()}-${file.originalname}` );
+    }
+})
+
+const filter = (req, file , cb) => {
+    cb(null , true); // tous les fichiers sont acceptés en upload
+    // cb(null , false); // tous les fichiers ne sont acceptés en upload
+}
+
+const upload = multer({
+        storage : strategie ,
+        limits : { fileSize : 1024 * 1024 * 6 }, // maximum peuvent avoir un point de 6 Mo
+        fileFilter : filter
+    }) 
 app.use("/" , express.static("public"))
 
 app.post("/" , upload.single("pdf") , (req, rep) => { 
@@ -16,7 +34,10 @@ app.post("/" , upload.single("pdf") , (req, rep) => {
     // on peut envoyer 1 seule image 
     // démarrer le serveur 
     // lancer postman 
-    rep.json("hello")
+    rep.json({
+        file : req.file, // ajoute une nouvelle propriété à req => file 
+        body : req.body  // pour les champs de type textuel 
+    })
 })
 
 app.listen(PORT , () => {console.log("express start sur port " + PORT) }) 
