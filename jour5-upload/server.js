e724c1aba5b905d800b1d007605d901099ceca1c
Malik H <malik.h@webdevpro.net>
2021-11-19 14:17:28 +0100

    refaire la fonctionnalité pstman

--- jour5-upload/server.js
+++ jour5-upload/server.js
@@ -2,10 +2,20 @@
 // npm i express multer 
 
 const express = require("express");
+const multer = require("multer");
+
 const PORT = process.env.PORT || 3210
 const app = express()
+// middleware 
+const upload = multer({dest : "images/"}) // chemin relatif dans le dossier en cours 
 
-app.post("/" , (req, rep) => {
+app.use("/" , express.static("public"))
+
+app.post("/" , upload.single("pdf") , (req, rep) => { 
+    // dans notre requête POST il y aura un champ image => fichier image 
+    // on peut envoyer 1 seule image 
+    // démarrer le serveur 
+    // lancer postman 
     rep.json("hello")
 })
 
